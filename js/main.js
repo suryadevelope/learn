762cee5ea0ffccaacbae970fd28f2ba915befdaa
suryadevelope <suryaprakashlokula@gmail.com>
2020-07-01 16:29:43 +0530

    ok

--- js/main.js
+++ js/main.js
@@ -13,3 +13,33 @@ document.getElementById("right").style.display='block'
     
 
 });
+
+var uplodbtn = document.getElementById("upload");
+var namebx = document.getElementById("name");
+var age = document.getElementById("age");
+var getdata = document.getElementById("get");
+
+const database = firebase.database();
+
+uplodbtn.addEventListener('click',function(){
+    database.ref('/data/').child(namebx.value).set({
+      namebx: age.value
+    
+
+    });
+
+});
+
+getdata.addEventListener('click',function(){
+  database.ref('/data/').orderByChild('namebx').on('value',snapshot=> {
+
+       console.log(snapshot.val());
+  });
+
+});
+
+database.ref('/data/').on('child_added',snapshot => {
+console.log(snapshot.child());
+
+});
+
