8ddbc12f8694b51fe3abf4cac1103b069de44f65
suryadevelope <suryaprakashlokula@gmail.com>
2020-07-01 13:39:00 +0530

    changed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 document.getElementById("btn").addEventListener('click',function(){
+document.getElementById("left").style.display='block'
+document.getElementById("right").style.display='none'
 
-    window.location="https://www.google.com";
+    
+
+});
+document.getElementById("btn1").addEventListener('click',function(){
+    document.getElementById("left").style.display='none'
+document.getElementById("right").style.display='block'
+
+
+    
 
 });
