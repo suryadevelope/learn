d654cc1f52c140a88c9eb51dd09fff7646936ab7
suryadevelope <suryaprakashlokula@gmail.com>
2020-07-02 10:42:55 +0530

    phone auth firebase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+
+
 document.getElementById("btn").addEventListener('click',function(){
 document.getElementById("left").style.display='block'
 document.getElementById("right").style.display='none'
@@ -18,8 +21,81 @@ var uplodbtn = document.getElementById("upload");
 var namebx = document.getElementById("name");
 var age = document.getElementById("age");
 var getdata = document.getElementById("get");
+var phone = document.getElementById("phone");
+var otpbx = document.getElementById("otptx");
+var getotp = document.getElementById("otp");
+var gototp = document.getElementById("otpbtn");
 
 const database = firebase.database();
+var appVerifier;
+
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+  "recaptcha-container",
+  {
+    size: "normal",
+    callback: function(response) {
+      console.log("recaptha completed: "+response);
+      getotpnu();
+    
+      
+    }
+  }
+);
+
+
+getotp.addEventListener('click',function(){
+  getotpnu();
+
+});
+function getotpnu(){
+
+  if(phone.value !== null){
+    // We are using the test phone numbers we created before
+// var phoneNumber = document.getElementById("phoneNumber").value;
+console.log("started number");
+
+var phoneNumber = "+91"+phone.value;
+appVerifier = window.recaptchaVerifier;
+
+firebase
+.auth()
+.signInWithPhoneNumber(phoneNumber, appVerifier)
+.then(function(confirmationResult) {
+   window.confirmationResult = confirmationResult;
+   console.log("otpsend");
+   document.getElementById("recaptcha-container").style.display='none';
+})
+.catch(function(error) {
+   console.log("cant get otp: "+error);
+});
+}
+
+}
+
+
+
+
+
+gototp.addEventListener('click',function(){
+
+  if(otpbx !== null) {
+    // We are using the test code we created before
+    // var code = document.getElementById("code").value;
+    var code = otpbx.value;
+    console.log("otp: "+code);
+    confirmationResult
+    .confirm(code)
+    .then(function(result) {
+        var user = result.uid;
+        console.log("user data: "+user);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+  }
+
+});
+
 
 uplodbtn.addEventListener('click',function(){
     database.ref('/data/').child(namebx.value).set({
@@ -43,3 +119,15 @@ console.log(snapshot.child());
 
 });
 
+window.onload = function(){
+  
+firebase.auth().onAuthStateChanged(function(user){
+  if(user){
+    //if user has signed in already
+     var uid = user.uid;
+     console.log("uid: "+uid);
+
+  }
+  
+});
+}
